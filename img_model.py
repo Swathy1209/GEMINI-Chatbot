07ab938f77611810f0ee20ae209cc11005381019
Swathy1209 <162039281+Swathy1209@users.noreply.github.com>
2025-02-13 10:47:47 +0530

    Update img_model.py

--- img_model.py
+++ img_model.py
@@ -12,7 +12,7 @@ import google.generativeai as genai
 from IPython.display import display
 from IPython.display import Markdown
 
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAWGXuxsmGUhWnBzfj0dy9-zv61BHIH7i8'
+os.environ['GOOGLE_API_KEY'] = 
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 def get_gemini_response(input,image):
